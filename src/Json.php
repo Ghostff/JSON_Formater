95c9862ffb3d025f5ac240c53d0d3851b8efad7a
ghostff <frankchris@hotmail.com>
2016-09-10 19:35:27 -0500

    PSR

--- src/Json.php
+++ src/Json.php
@@ -9,36 +9,36 @@ class Json
         $new_string = '<div style="font-family:Courier New, Courier, monospace;">';
                    
         $pathern = array(
-			'/\{/',
-			'/\}/',
-			'/(",|\d,|true(,|)|false(,|)|\},|null,)/'
+            '/\{/',
+            '/\}/',
+            '/(",|\d,|true(,|)|false(,|)|\},|null,)/'
          );
-		 
+         
         $replacement = array(
-			'$0<br /><div style=\'margin-left:20px;\'>',
-			'</div>$0',
-			'$0<br />'
-		);
-		
+            '$0<br /><div style=\'margin-left:20px;\'>',
+            '</div>$0',
+            '$0<br />'
+        );
+        
         $string = preg_replace($pathern, $replacement, $string);
         
         foreach (explode('<br />', $string) as $nw_string)
-		{
+        {
             $new_string .= preg_replace(
-				'/"(.*?)\":/',
-				'"$1": ',
-				$nw_string
-			);
-			
-			$new_string .= '<br />';
+                '/"(.*?)\":/',
+                '"$1": ',
+                $nw_string
+            );
+            
+            $new_string .= '<br />';
         }
-		
+        
         $new_string = str_replace(
-			array('>: ', '\/'),
-			array(">: </span>", '/'),
-			$new_string
-		);
-		
+            array('>: ', '\/'),
+            array(">: </span>", '/'),
+            $new_string
+        );
+        
         $new_string .= '</div>';
         
         return $new_string;
